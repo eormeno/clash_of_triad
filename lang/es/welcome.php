cd0120b10c8c68f7728457277eb146ca06b1dde3
Emilio Ormeño <eormneno@gmail.com>
2024-04-10 22:41:26 -0300

    Idioma español en plantillas de login y bienvenida

---
+++ lang/es/welcome.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'welcome_title' => 'Bienvenido a la sala de juegos!',
+    'welcome_subtitle' => 'Aquí puedes jugar a los juegos que quieras, cuando quieras.',
+    'documentation' => 'Documentación',
+    'documentation_text' => 'Aquí puedes encontrar la documentación de los juegos y las instrucciones de uso.',
+    'videos' => 'Videos',
+    'videos_text' => 'Aquí puedes encontrar videos tutoriales de los juegos.',
+    'credits' => 'Créditos',
+    'credits_text' => 'Aquí puedes encontrar los créditos de los juegos y de la plataforma.',
+    'games' => 'Juegos',
+    'games_text' => 'Aquí puedes encontrar los juegos disponibles en la plataforma.',
+];
