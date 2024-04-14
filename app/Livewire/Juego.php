f04f2bcc470bf00828c20e12cae48df9bc3c2a0e
Emilio Ormeño <eormneno@gmail.com>
2024-04-13 21:48:15 -0300

    game states

--- app/Livewire/Juego.php
+++ app/Livewire/Juego.php
@@ -7,21 +7,50 @@ use Livewire\Component;
 class Juego extends Component
 {
 
-    public $count = 1;
+    private $estadosDeJuego = [
+        'Buscando oponente',
+        'Elige tu jugada',
+        'Esperando a tu oponente',
+        'Resultado de la partida',
+    ];
+
+    public $indiceEstadoActual = 0;
+
+    public $estadoDeJuego;
+
+    public $jugador = '';
+
+    public $choice = -1;
+
+    public function mount()
+    {
+        $this->jugador = auth()->user()->name;
+        $this->updateState();
+    }
+
+    public function updateState()
+    {
+        $countStates = count($this->estadosDeJuego);
+        if ($this->indiceEstadoActual > $countStates - 1) {
+            $this->indiceEstadoActual = 0;
+        }
+        $this->estadoDeJuego = $this->estadosDeJuego[$this->indiceEstadoActual];
+        $this->indiceEstadoActual++;
+    }
 
     public function rock()
     {
-        $this->count++;
+        $this->choice = 0;
     }
 
     public function paper()
     {
-        $this->count--;
+        $this->choice = 1;
     }
 
     public function scissors()
     {
-        $this->count = 1;
+        $this->choice = 2;
     }
 
     public function render()
