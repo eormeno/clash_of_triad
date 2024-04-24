ae465f38cb414b7885546ead1af2cd1900b190a9
DAMO\Emilio <eormeno@gmail.com>
2024-04-24 08:49:02 -0300

    template improvements

--- resources/views/livewire/juego.blade.php
+++ resources/views/livewire/juego.blade.php
@@ -1,11 +1,6 @@
 <div
     class="p-6 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
 
-    <button wire:click="clear"
-        class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-gray-700 border border-transparent rounded-md shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-        Clear
-    </button>
-
     <!--
     <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
         {{ __('Jugador') }}: {{ $jugador }}
@@ -19,46 +14,75 @@
         -->
     </div>
 
-    <div x-show="$wire.estadoActual === 'buscando oponente'" class="mt-6 align-middle">
-        <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-            {{ __('Esperando al otro jugador') }}
-        </h1>
-    </div>
+    @switch($estadoActual)
+        @case('buscando oponente')
+            <div class="mt-6 align-middle">
+                <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+                    {{ __('Buscando oponente') }}: {{ $remainingTime }}
+                </h1>
+            </div>
+        @break
 
-    <div x-show="$wire.estadoActual === 'pedir jugada'" class="mt-6 align-middle">
-        <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-            {{ __('Ronda') }} : {{ $ronda }}
-        </h1>
-        <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-            {{ __('Elige tu jugada') }}
-        </h1>
-        <button wire:click="play(0)"
-            class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-red-700 border border-transparent rounded-md shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">Papel</button>
-        <button wire:click="play(1)"
-            class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-green-700 border border-transparent rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-geen-500">Piedra</button>
-        <button wire:click="play(2)"
-            class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-700 border border-transparent rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Tijera</button>
-    </div>
+        @case ('oponente encontrado')
+            <div class="mt-6 align-middle">
+                <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+                    {{ __('Oponente encontrado') }}
+                </h1>
+            </div>
+        @break
 
-    <div x-show="$wire.estadoActual === 'mostrar resultado ronda'" class="mt-6 align middle">
-        <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-            {{ __('Resultado') }} : {{ $resultadoRonda }}
-        </h1>
-    </div>
+        @case('pedir jugada')
+            <div class="mt-6 align-middle">
+                <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+                    {{ __('Ronda') }} : {{ $ronda }}
+                </h1>
+                <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+                    {{ __('Elige tu jugada') }}
+                </h1>
+                <button wire:click="play(0)"
+                    class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-red-700 border border-transparent rounded-md shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">Papel</button>
+                <button wire:click="play(1)"
+                    class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-green-700 border border-transparent rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-geen-500">Piedra</button>
+                <button wire:click="play(2)"
+                    class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-blue-700 border border-transparent rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Tijera</button>
+            </div>
+        @break
 
-    <div x-show="$wire.estadoActual === 'mostrar número ronda'" class="mt-6 align middle">
-        <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-            {{ __('Ronda') }} : {{ $ronda }}
-        </h1>
-    </div>
+        @case('mostrar resultado ronda')
+            <div class="mt-6 align middle">
+                <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+                    {{ __('Resultado') }} : {{ $resultadoRonda }}
+                </h1>
+            </div>
+        @break
 
-    <div x-show="$wire.estadoActual === 'mostrar resultado juego'" class="mt-6 align middle">
-        <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-            {{ __('Tu puntaje: ') }} {{ $puntajeJugador }}
-        </h1>
-        <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-            {{ __('Puntaje oponente: ') }} {{ $puntajeOponente }}
-        </h1>
-    </div>
+        @case('mostrar número ronda')
+            <div class="mt-6 align middle">
+                <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+                    {{ __('Ronda') }} : {{ $ronda }}
+                </h1>
+            </div>
+        @break
+
+        @case('mostrar resultado juego')
+            <div class="mt-6 align middle">
+                <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+                    {{ __('Tu puntaje: ') }} {{ $puntajeJugador }}
+                </h1>
+                <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+                    {{ __('Puntaje oponente: ') }} {{ $puntajeOponente }}
+                </h1>
+            </div>
+        @break
+
+        @case ('fin')
+            <button wire:click="clear"
+                class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-gray-700 border border-transparent rounded-md shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                Jugar de nuevo
+            </button>
+        @break
+
+        @default
+    @endswitch
 
 </div>
